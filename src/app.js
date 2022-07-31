e5960df38e6b4622e6f2f62045f59f49bca2cb9f
aswarth <m.aswarthreddy@gmail.com>
2022-07-31 08:30:11 +0530

    added teacher load and teacher selection dropdown

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
+var data;
 async function drawChart(rows) {
   var container = document.getElementById("timeline");
   var chart = new google.visualization.Timeline(container);
   var dataTable = new google.visualization.DataTable();
 
   var days = ["Monday"];
-  dataTable.addColumn({ type: "string", id: "Room" });
+  dataTable.addColumn({ type: "string", id: "Day" });
   dataTable.addColumn({ type: "string", id: "Name" });
   dataTable.addColumn({ type: "date", id: "Start" });
   dataTable.addColumn({ type: "date", id: "End" });
@@ -14,48 +15,85 @@ async function drawChart(rows) {
   }
   //console.log(rows)
   //dataTable.addRows(rows);
-  
+
   google.visualization.events.addListener(chart, "ready", changeBorderRadius);
   google.visualization.events.addListener(chart, "select", changeBorderRadius);
-  google.visualization.events.addListener(
-    chart,
-    "onmouseover",
-    changeBorderRadius
-    );
-    google.visualization.events.addListener(
-      chart,
-      "onmouseout",
-      changeBorderRadius
-      );
-      
-      function changeBorderRadius() {
-        let borderRadius = 4;
+  google.visualization.events.addListener(chart, "onmouseover", changeBorderRadius);
+  google.visualization.events.addListener(chart, "onmouseout", changeBorderRadius);
+
+  function changeBorderRadius() {
+    let borderRadius = 4;
     chartColumns = container.getElementsByTagName("rect");
     Array.prototype.forEach.call(chartColumns, function (column) {
       if (
         column.getAttribute("fill") != "none" &&
         column.getAttribute("stroke") != "1"
-        ) {
+      ) {
         column.setAttribute("rx", borderRadius);
         column.setAttribute("ry", borderRadius);
       }
     });
   }
-  
+
   var options = {
-    backgroundColor: '#f7f7f7',
+    backgroundColor: "#f7f7f7",
     alternatingRowStyle: false,
-};
+  };
 
   chart.draw(dataTable, options);
 }
 
-function getTime(a, b=0) {
-  a = parseInt(a)
-  var hr = 9 + Math.floor((a+b) / 60);
-  var min = (a+b) % 60
+function getTime(a, b = 0) {
+  a = parseInt(a);
+  var hr = 9 + Math.floor((a + b) / 60);
+  var min = (a + b) % 60;
   //console.log(a, b, hr, min)
   return new Date(0, 0, 0, hr, min, 0);
+}
+
+function populateDropdown(data) {
+  let teacherList = "";
+
+  for (var teacher in data) {
+    teacherList += '<option value="' + teacher + '">' + teacher + "</option>";
+  }
+  document.getElementById("listTeach").innerHTML = teacherList;
+}
+
+function populateChartData(choice) {
+  let rows = [];
+  let teacherData = data[choice];
+  for (var days in teacherData) {
+    //for each day in week
+    var day = teacherData[days];
+    let teacherLoad = 0;
+    //console.log(data.sindhu[days]);
+    for (var key in day) {
+      //for each class in day
+      var val = day[key];
+
+      //key is start time
+      //val contains data
+
+      //console.log(key);
+      //console.log(val.start[0]);
+      teacherLoad += val.duration;
+      rows[rows.length] = [
+        days,
+        val.class,
+        getTime(key),
+        getTime(key, val.duration),
+      ];
+    }
+    rows[rows.length] = [
+      days,
+      String(teacherLoad / 60) + " Hours",
+      getTime(9 * 60),
+      getTime(9 * 60, teacherLoad / 5),
+    ];
+  }
+
+  drawChart(rows);
 }
 
 const fetchData = async (url) => {
@@ -65,42 +103,12 @@ const fetchData = async (url) => {
 };
 
 const main = async () => {
-  let rows = [];
-  const data = await fetchData("data.json");
-  for (days in data.sindhu) {
-    var day = data.sindhu[days];
-    //console.log(data.sindhu[days]);
-    for (var key in data.sindhu[days]) {
-      var val = day[key];
+  data = await fetchData("data.json");
+  populateDropdown(data);
 
-      //key is start time 
-      //val contains data
-      
-      //console.log(key);
-      //console.log(val.start[0]);
-      rows[rows.length] = [
-        days,
-        val.class,
-        getTime(key),
-        getTime(key, val.duration),
-      ];
-    }
-  }
+  populateChartData(Object.keys(data)[0]);
 
-  for (var key in data.sindhu.tuesday) {
-    var val = data.sindhu.tuesday[key];
-    //console.log(key);
-    //console.log(val.start[0]);
-    rows[rows.length] = [
-      "tuesday",
-      key,
-      getTime(val.start[0], val.start[1]),
-      getTime(val.end[0], val.end[1]),
-    ];
-  }
   //console.log(rows, "ok")
-
-  drawChart(rows);
 };
 
 //main();//removed because it randomly throws an error when called before chart lib is loaded
